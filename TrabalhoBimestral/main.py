e9841535a5eea93c348e6c5d129f1d947426be5a
Joao Conti <joaovitorconti@live.com>
2023-10-10 05:00:13 -0300

    Implementadas as funções com os botões

--- TrabalhoBimestral/main.py
+++ TrabalhoBimestral/main.py
@@ -1,13 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import cv2
 from vectorOfMats import VectorOfMats
 
 vec_of_mats = VectorOfMats()
-
-# Função para fechar a janela
-def fechar_janela():
-    janela.destroy()
+vec_of_mats2 = VectorOfMats()  # Crie uma nova instância para armazenar imagens temporariamente
 
 
 # Função para carregar uma imagem
@@ -26,7 +24,161 @@ def carregar_imagem():
         label_imagem.configure(image=image_tk)
         label_imagem.image = image_tk  # Mantenha uma referência para evitar que a imagem seja coletada pelo garbage collector
 
+def salvar_imagem():
+    if label_imagem.image:  # Verifique se há uma imagem atualmente exibida
+        imagem_atual = ImageTk.getimage(label_imagem.image)  # Obtenha a imagem exibida
+        if imagem_atual:
+            # Converta a imagem para o modo RGB antes de salvar
+            imagem_atual_rgb = imagem_atual.convert("RGB")
+            file_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=(("JPEG files", "*.jpg"), ("All files", "*.*")))
+            if file_path:
+                imagem_atual_rgb.save(file_path)
+                print("Imagem atual salva em:", file_path)
 
+def atualizar_imagem_exibida(imagem):
+    # Converta a imagem de BGR para RGB
+    imagem_rgb = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+    # Atualize a imagem exibida na janela com a imagem RGB
+    imagem_pil = Image.fromarray(imagem_rgb)
+    imagem_pil = imagem_pil.resize((frame_direito.winfo_width() - 40, frame_direito.winfo_height() - 40))
+    image_tk = ImageTk.PhotoImage(image=imagem_pil)
+    label_imagem.configure(image=image_tk)
+    label_imagem.image = image_tk
+
+
+def exibir_imagem_anterior():
+    if len(vec_of_mats.images) > 1:  # Verifique se há pelo menos duas imagens no vetor
+        # Remova a imagem atual do vetor e adicione a imagem removida em vec_of_mats2
+        imagem_anterior = vec_of_mats.images.pop()
+        vec_of_mats2.images.append(imagem_anterior)
+        # Obtenha a imagem anterior
+        imagem_anterior = vec_of_mats.images[-1]
+        # Atualize a imagem exibida na janela com a imagem anterior
+        atualizar_imagem_exibida(imagem_anterior)
+
+def exibir_proxima_imagem():
+    if len(vec_of_mats2.images) > 0:  # Verifique se há imagens em vec_of_mats2
+        # Remova a imagem mais recente de vec_of_mats2 e adicione-a de volta a vec_of_mats
+        imagem_proxima = vec_of_mats2.images.pop()
+        vec_of_mats.images.append(imagem_proxima)
+        # Atualize a imagem exibida na janela com a imagem recuperada
+        atualizar_imagem_exibida(imagem_proxima)
+
+def delete_imagem():
+    if len(vec_of_mats.images) > 0:
+        # Remova a imagem atual do vetor
+        vec_of_mats.images.pop()
+        # Verifique se ainda há imagens no vetor
+        if len(vec_of_mats.images) > 0:
+            # Obtenha a imagem anterior
+            imagem_anterior = vec_of_mats.images[-1]
+            # Atualize a imagem exibida na janela com a imagem anterior
+            atualizar_imagem_exibida(imagem_anterior)
+        else:
+            # Se não houver mais imagens no vetor, limpe a imagem exibida
+            label_imagem.configure(image=None)
+
+
+
+
+
+
+
+
+
+
+
+
+def converter_cor(imagem):
+    # Aplicar a conversão de cor na imagem (por exemplo, converter para tons de cinza)
+    imagem_convertida = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    # Converta a imagem de BGR para RGB antes de armazenar no vetor
+    imagem_convertida_rgb = cv2.cvtColor(imagem_convertida, cv2.COLOR_BGR2RGB)
+    return imagem_convertida_rgb
+def aplicar_conversao_cor():
+    if vec_of_mats.images:
+        imagem_original = vec_of_mats.images[-1]  # Obtenha a imagem mais recente do vetor
+        imagem_convertida = converter_cor(imagem_original)  # Aplique a conversão de cor
+        vec_of_mats.images.append(imagem_convertida)  # Armazene a imagem convertida no vetor
+        # Atualize a imagem exibida na janela com a imagem de bordas
+        print("Imagem convertida e armazenada no vetor.")
+        atualizar_imagem_exibida(imagem_convertida)
+
+
+def apply_filter(imagem):
+    imagem_convertida_bgr = cv2.cvtColor(imagem, cv2.COLOR_RGB2BGR)
+    # Aplique o filtro à imagem atual, você pode aplicar um filtro de suavização da seguinte forma:
+    imagem_convertida = cv2.GaussianBlur(imagem_convertida_bgr, (5, 5), 0)
+    # Converta a imagem de BGR para RGB antes de armazenar no vetor
+    imagem_convertida_rgb = cv2.cvtColor(imagem_convertida, cv2.COLOR_BGR2RGB)
+    return imagem_convertida_rgb
+def aplicar_filtro():
+    if vec_of_mats.images:
+        imagem_original = vec_of_mats.images[-1]  # Obtenha a imagem mais recente do vetor
+        imagem_convertida = apply_filter(imagem_original)  # Aplique a conversão de cor
+        vec_of_mats.images.append(imagem_convertida)  # Armazene a imagem convertida no vetor
+        # Atualize a imagem exibida na janela com a imagem de bordas
+        print("Aplicado o filtro e armazenada no vetor.")
+        atualizar_imagem_exibida(imagem_convertida)
+
+
+def aplicar_detecao(imagem):
+    # Aplique a detecção de bordas usando o operador Canny
+    imagem_convertida = cv2.Canny(imagem, 100, 200)
+    # Converta a imagem de BGR para RGB antes de armazenar no vetor
+    imagem_convertida_rgb = cv2.cvtColor(imagem_convertida, cv2.COLOR_BGR2RGB)
+    return imagem_convertida_rgb
+def detectar_bordas():
+    if vec_of_mats.images:
+        imagem_original = vec_of_mats.images[-1]  # Obtenha a imagem mais recente do vetor
+        imagem_convertida = aplicar_detecao(imagem_original) # Você pode ajustar os valores 100 e 200 conforme necessário
+        vec_of_mats.images.append(imagem_convertida)
+        # Atualize a imagem exibida na janela com a imagem de bordas
+        print("Imagem com bordas detectadas e armazenada no vetor.")
+        atualizar_imagem_exibida(imagem_convertida)
+
+
+def aplicar_binarizacao(imagem):
+     # Converta a imagem para tons de cinza (se já não estiver em tons de cinza)
+        if len(imagem.shape) == 3:
+            imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+        # Aplique a binarização da imagem
+        _, imagem_convertida = cv2.threshold(imagem, 128, 255, cv2.THRESH_BINARY)
+        # Converta a imagem de BGR para RGB antes de armazenar no vetor
+        imagem_convertida_rgb = cv2.cvtColor(imagem_convertida, cv2.COLOR_BGR2RGB)
+        return imagem_convertida_rgb
+def binarizar_imagem():
+    if vec_of_mats.images:
+        imagem_original = vec_of_mats.images[-1]  # Obtenha a imagem mais recente do vetor
+        imagem_convertida = aplicar_binarizacao(imagem_original) # Você pode ajustar os valores 100 e 200 conforme necessário
+        vec_of_mats.images.append(imagem_convertida)
+        # Atualize a imagem exibida na janela com a imagem de bordas
+        print("Imagem binarizada e armazenada no vetor.")
+        atualizar_imagem_exibida(imagem_convertida)
+
+
+def apply_morphology(imagem):
+        # Certifique-se de que a imagem está binarizada (use o valor adequado de limiar)
+        _, imagem_binaria = cv2.threshold(imagem, 128, 255, cv2.THRESH_BINARY)
+        # Defina o kernel para a operação de erosão (pode ajustar o tamanho do kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        # Aplique a operação de erosão
+        imagem_convertida = cv2.erode(imagem_binaria, kernel, iterations=1)
+        # Converta a imagem de BGR para RGB antes de armazenar no vetor
+        imagem_convertida_rgb = cv2.cvtColor(imagem_convertida, cv2.COLOR_BGR2RGB)
+        return imagem_convertida_rgb
+def aplicar_morfologia():
+    if vec_of_mats.images:
+        imagem_original = vec_of_mats.images[-1]  # Obtenha a imagem mais recente do vetor
+        imagem_convertida = apply_morphology(imagem_original) # Você pode ajustar os valores 100 e 200 conforme necessário
+        vec_of_mats.images.append(imagem_convertida)
+        # Atualize a imagem exibida na janela com a imagem de bordas
+        print("Aplicada a morfologia e armazenada no vetor.")
+        atualizar_imagem_exibida(imagem_convertida)
+
+
+
+# Crie a janela principal
 janela = tk.Tk()
 janela.title("Minha Janela")
 janela.geometry("800x600")
@@ -34,8 +186,6 @@ janela.geometry("800x600")
 janela.grid_rowconfigure(0, weight=1)
 janela.columnconfigure(0, weight=1)
 janela.columnconfigure(1, weight=4)
-
-
 
 
 # Parte esquerda (20% da largura)
@@ -46,28 +196,28 @@ largura_botoes = 15
 # Adicione seus botões ao frame_esquerdo
 botao1 = tk.Button(frame_esquerdo, text="Carregar imagem", width=largura_botoes, command=carregar_imagem)
 botao1.grid(row=0, column=0, padx=10, pady=(10,40))
-botao2 = tk.Button(frame_esquerdo, text="convert_color", width=largura_botoes)
+botao2 = tk.Button(frame_esquerdo, text="convert_color", width=largura_botoes, command=aplicar_conversao_cor)
 botao2.grid(row=1, column=0, padx=10, pady=10)
-botao3 = tk.Button(frame_esquerdo, text="apply_filter", width=largura_botoes)
+botao3 = tk.Button(frame_esquerdo, text="apply_filter", width=largura_botoes, command=aplicar_filtro)
 botao3.grid(row=2, column=0, padx=10, pady=10)
-botao4 = tk.Button(frame_esquerdo, text="detect_edges", width=largura_botoes)
+botao4 = tk.Button(frame_esquerdo, text="detect_edges", width=largura_botoes, command=detectar_bordas)
 botao4.grid(row=3, column=0, padx=10, pady=10)
-botao5 = tk.Button(frame_esquerdo, text="binarize", width=largura_botoes)
+botao5 = tk.Button(frame_esquerdo, text="binarize", width=largura_botoes, command=binarizar_imagem)
 botao5.grid(row=4, column=0, padx=10, pady=10)
-botao6 = tk.Button(frame_esquerdo, text="apply_morphology", width=largura_botoes)
+botao6 = tk.Button(frame_esquerdo, text="apply_morphology", width=largura_botoes, command=aplicar_morfologia)
 botao6.grid(row=5, column=0, padx=10, pady=(10, 40))
-botao_salvar = tk.Button(frame_esquerdo, text="Salvar Imagem", width=largura_botoes)
+botao_salvar = tk.Button(frame_esquerdo, text="Salvar Imagem", width=largura_botoes, command=salvar_imagem)
 botao_salvar.grid(row=6, column=0, padx=10, pady=10)
 # Crie um novo Frame dentro do frame esquerdo para os botões "Next" e "Previous"
 frame_botoes_nav = tk.Frame(frame_esquerdo, bg="light gray")
 frame_botoes_nav.grid(row=7, column=0, padx=10, pady=10)
 # Configure os botões "Next" e "Previous"
-botao_previous = tk.Button(frame_botoes_nav, text="  <  ", width=5)
+botao_previous = tk.Button(frame_botoes_nav, text="  <  ", width=5, command=exibir_imagem_anterior)
 botao_previous.pack(side="left", padx=5)
-botao_next = tk.Button(frame_botoes_nav, text="  >  ", width=5)
+botao_next = tk.Button(frame_botoes_nav, text="  >  ", width=5, command=exibir_proxima_imagem)
 botao_next.pack(side="left", padx=5)
-
-
+botao_delete = tk.Button(frame_esquerdo, text="  X  ", width=5, command=delete_imagem)
+botao_delete.grid(row=8, column=0, padx=10, pady=10)
 
 # Parte direita (80% da largura)
 frame_direito = tk.Frame(janela, bg="white")
@@ -81,6 +231,9 @@ label_imagem.pack(fill="both", expand=True)
 
 
 
+# Função para fechar a janela
+def fechar_janela():
+    janela.destroy()
 # Configure o botão para fechar a janela
 janela.protocol("WM_DELETE_WINDOW", fechar_janela)
 # Use o weight para que as colunas se expandam com o redimensionamento da janela
